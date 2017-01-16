bfe8a9b206158e9a3687130a6c8040eab8f5141a
aelmi <aelmi@ukp.informatik.tu-darmstadt.de>
2017-01-16 22:28:42 +0100

    eval dataset 2

--- packages/main.py
+++ packages/main.py
@@ -17,6 +17,7 @@ testPath = config.test2Path
 # Train one model per language
 for file_name in os.listdir(langPath):
     if file_name.__contains__(".txt"):
+        print(file_name)
         model_name = file_name[:-4]
         if model_name in ["xx"]:
             continue
@@ -33,8 +34,9 @@ for file_name in os.listdir(langPath):
                 with open("{0}/{1}/{2}".format(modelPath, n_gram, file_name), 'wb') as model_file_handler:
                     pk.dump(model, model_file_handler)
                 models[model_name] = model
+        print('done')
 
-
+print('learning done')
 # Change models configuration
 # for model_name in models:
 #     models[model_name].limit_model(400)
@@ -42,12 +44,16 @@ for file_name in os.listdir(langPath):
 
 #Test
 wrong_list = []
+test_buffer_len = []
 true_positive = 0
 counter = 0
 
 s = 1
 for file_name in os.listdir(testPath):
+    print(file_name)
     true_lang = file_name[-2:]
+    if true_lang in ["el", "gz", ".e"]:
+        continue
     with file("{0}/{1}".format(testPath,file_name), 'rt') as test_file_handler:
         _buffer = test_file_handler.readlines()
         for i in xrange(0, len(_buffer), s):
@@ -60,7 +66,7 @@ for file_name in os.listdir(testPath):
                     max_lang = lang
                     max_score = score
 
-            print(true_lang, max_lang, max_score)
+            print(true_lang, max_lang, max_score, len(test_buffer))
 
             if true_lang == max_lang:
                 true_positive += 1
